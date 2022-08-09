855495aa7b0bcf99e1dcbb60ada34c283fd6093c
David Brown <2047439+dugbus@users.noreply.github.com>
2022-08-09 18:33:11 +0100

    updated waveform color mode

--- p5/waveform/sketch.js
+++ p5/waveform/sketch.js
@@ -1,47 +1,55 @@
-let y = 0;
-let lineHeight;
-let rectWidth = 1;
 let II = false;
 
 let c1, c2;
 
 let waves = [];
 
+let x, y, mid;
+
+const step = 3;
+
 const screenWidth = document.body.clientWidth
 const screenHeight = document.body.clientHeight
 
 function setup() {
   createCanvas(screenWidth, screenHeight);
-  frameRate(30);
   fillArray();
-  c1 = color(204, 102, 0);
-  c2 = color(0, 102, 153);
-  noLoop();
+  background(22, 22, 29);
+  noStroke();
+  noSmooth();
 }
 
 function draw() {
-  for (let i = 0; i < width; i++) {
-    for (let j = height / 2 - waves[i] / 2; j < waves[i]; j++) {
-      stroke(255, 255, 255);
-      point(i, j);
-    }
+  for (let y = 0; y < waves[x]; y = y + step) {
+    fill(100, 204, 240, y / 20 + 15);
+    circle(x, mid + y, 5);
+    fill(204, 204, 100, y / 20 + 15);
+    circle(x, mid - y, 5);
+  }
+  x = x + step;
+  if (x > width) {
+    fillArray();
   }
 }
 
-function drawGradient(x, y, w, h, c1, c2, axis) {
-  for (let i = y; i <= y + h; i++) {
-    let inter = map(i, y, y + h, 0, 1);
-    let c = lerpColor(c1, c2, inter);
-    stroke(c);
-    line(x, i, x + w, i);
-  }
-}
+// function drawGradient(x, y, w, h, c1, c2, axis) {
+//   for (let i = y; i <= y + h; i++) {
+//     let inter = map(i, y, y + h, 0, 1);
+//     let c = lerpColor(c1, c2, inter);
+//     stroke(c);
+//     line(x, i, x + w, i);
+//   }
+// }
 
 function fillArray() {
-  background(22, 22, 29);
+
+  x = 0;
+  y = 0;
+  mid = height / 2;
+
   waves[0] = random(screenHeight / 2);
-  for (i = 1; i < screenWidth; i++) {
-    waves[i] = waves[i - 1] + random(-5, 5);
+  for (let i = 1; i < screenWidth; i++) {
+    waves[i] = waves[i - 1] + random(-2, 2);
   }
 }
 
