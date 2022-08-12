7b0ba0b2cd9c8cc93112402c18ab2d2281f60639
David Brown <2047439+dugbus@users.noreply.github.com>
2022-08-12 12:05:05 +0100

    removed push from fillArray

--- p5/ripples/sketch.js
+++ p5/ripples/sketch.js
@@ -22,15 +22,28 @@ function draw() {
 function fillArray() {
   background(22, 22, 29);
   hoops.length = 0;
+  randomSeed(47);
   for (let i = 0; i < count; i++) {
-    let hoop = {}
-    hoop.x = random(width);
-    hoop.y = random(height);
-    hoop.r = random(0);
-    hoop.s = random(3);
-    hoops.push(hoop);
+    hoops[i] = {};
+    hoops[i].x = random(width);
+    hoops[i].y = random(height);
+    hoops[i].r = random(0);
+    hoops[i].s = random(3);
   }
 }
+
+// function fillArray() {
+//   background(22, 22, 29);
+//   hoops.length = 0;
+//   for (let i = 0; i < count; i++) {
+//     let hoop = {}
+//     hoop.x = random(width);
+//     hoop.y = random(height);
+//     hoop.r = random(0);
+//     hoop.s = random(3);
+//     hoops.push(hoop);
+//   }
+// }
 
 function showHoop(hoop) {
   circle(hoop.x, hoop.y, hoop.r);
